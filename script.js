311947ece30e6da766d8db183ec1bce753404247
chitrangna <chitrangnao901@gmail.com>
2026-07-19 17:25:23 +0530

    Implement task management with local storage

--- script.js
+++ script.js
@@ -1,1 +1,106 @@
+const input = document.getElementById("taskInput");
 
+const addBtn = document.getElementById("addBtn");
+
+const list = document.getElementById("taskList");
+
+let tasks = JSON.parse(localStorage.getItem("tasks")) || [];
+
+function saveTasks(){
+
+localStorage.setItem(
+"tasks",
+JSON.stringify(tasks)
+);
+
+}
+
+function render(){
+
+list.innerHTML = "";
+
+tasks.forEach((task,index)=>{
+
+const li=document.createElement("li");
+
+const span=document.createElement("span");
+
+span.innerText=task.text;
+
+span.className="task";
+
+if(task.completed){
+
+span.classList.add("completed");
+
+}
+
+span.onclick=()=>{
+
+tasks[index].completed=!tasks[index].completed;
+
+saveTasks();
+
+render();
+
+};
+
+const del=document.createElement("button");
+
+del.className="delete";
+
+del.innerHTML='<i class="fas fa-trash"></i>';
+
+del.onclick=()=>{
+
+tasks.splice(index,1);
+
+saveTasks();
+
+render();
+
+};
+
+li.appendChild(span);
+
+li.appendChild(del);
+
+list.appendChild(li);
+
+});
+
+}
+
+addBtn.onclick=()=>{
+
+const text=input.value.trim();
+
+if(text==="") return;
+
+tasks.push({
+
+text:text,
+
+completed:false
+
+});
+
+saveTasks();
+
+render();
+
+input.value="";
+
+};
+
+input.addEventListener("keypress",(e)=>{
+
+if(e.key==="Enter"){
+
+addBtn.click();
+
+}
+
+});
+
+render();
